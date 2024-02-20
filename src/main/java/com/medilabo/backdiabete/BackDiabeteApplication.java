d108f9600d6c5773e9fc523a0fa73c9048476d85
DurandoDev <nat.durando@gmail.com>
2024-02-20 12:54:57 +0100

    first commit

---
+++ src/main/java/com/medilabo/backdiabete/BackDiabeteApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.backdiabete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDiabeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDiabeteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medilabo/backdiabete/BackDiabeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.backdiabete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDiabeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
