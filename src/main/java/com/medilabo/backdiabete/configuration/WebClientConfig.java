a4ae741062c9dd6063227108fdc103196f580ba9
DurandoDev <nat.durando@gmail.com>
2024-03-10 17:48:07 +0100

    Clean non use imports

--- src/main/java/com/medilabo/backdiabete/configuration/WebClientConfig.java
+++ src/main/java/com/medilabo/backdiabete/configuration/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.medilabo.backdiabete.configuration;
 
-import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
